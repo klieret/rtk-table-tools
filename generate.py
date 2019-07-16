41d6b988fffc48d70f6e9de17e5ce7cda7587848
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-07-16 17:50:05 +0200

    [Feat] Tangorin scraper

--- generate.py
+++ generate.py
@@ -3,10 +3,16 @@
 # std
 from typing import Union, List
 from pathlib import Path, PurePath
+import requests
+import time
+import json
+import collections
 
 # 3rd
 import pandas as pd
 import numpy as np
+from tqdm.auto import tqdm
+from bs4 import BeautifulSoup
 
 
 class Kanjis(object):
@@ -38,6 +44,83 @@ class Kanjis(object):
         return self.df["kanji"].values.tolist()
 
 
+class TangorinScraper(object):
+    def __init__(self, out_dir="scrape"):
+        self.out_dir = Path(out_dir)
+
+    def _build_url(self, kanji):
+        return "https://tangorin.com/kanji?search={}".format(kanji)
+
+    @staticmethod
+    def _download(url: str, path: Path) -> None:
+        with path.open("wb") as outfile:
+            r = requests.get(url)
+            outfile.write(r.content)
+
+    def download_kanji(self, kanji: str, force=False) -> bool:
+        path = self.out_dir / (str(ord(kanji)) + ".html")
+        if not force and path.exists():
+            tqdm.write("Skipping existing kanji {}".format(kanji))
+            return False
+        self._download(self._build_url(kanji), path)
+        return True
+
+    def download_kanjis(self, kanjis: List[str], timeout=3):
+        for kanji in tqdm(kanjis):
+            new_download = self.download_kanji(kanji)
+            if new_download:
+                time.sleep(timeout)
+
+
+class TangorinParser(object):
+    def __init__(self):
+        pass
+
+    def parse(self, path):
+        path = Path(path)
+        with path.open("r") as infile:
+            html = infile.read()
+        soup = BeautifulSoup(html, "html.parser")
+        for _script in soup.find_all("script"):
+            if not _script.string:
+                continue
+            if "window.__PRELOADED_STATE" in _script.string:
+                script = _script.string
+                break
+        else:
+            return {}
+        _, dct_str = script.split("window.__PRELOADED_STATE=")
+        dct_str = dct_str[:-1]  # split ;
+        dct = json.loads(dct_str)
+        dct = dct["search"][list(dct["search"].keys())[0]]["items"][0]["rows"][0]
+        out_dct = {}
+        renames = {
+            "jlpt": "jlpt",
+            "k": "kanji"
+        }
+        for old, new in renames.items():
+            try:
+                out_dct[new] = dct[old]
+            except KeyError:
+                out_dct[new] = None
+        return out_dct
+
+    def parse_dir(self, folder):
+        folder = Path(folder)
+        dct = collections.defaultdict(list)
+        for file in tqdm(list(folder.iterdir())):
+            if not file.is_file():
+                continue
+            for key, value in self.parse(file).items():
+                dct[key].append(value)
+        return dct
+
+    def save2csv(self, dct, path="tangorin.csv"):
+        path = Path(path)
+        df = pd.DataFrame(dct)
+        df.to_csv(path)
+
+
 class LatexKanjiTable(object):
     def __init__(self, k):
         self.ncols = 8
@@ -60,13 +143,14 @@ class LatexKanjiTable(object):
                "\\vspace{{{vadd}}}\n" \
                "{{\Huge {kanji} }}\n" \
                "\\\\[0.3ex]\n" \
-               "{id}\n" \
+               "{id} {utf}\n" \
                "\\vspace{{{vadd}}}\n" \
                "\\end{{minipage}}\n".format(
             width=self.cell_width,
             vadd=self.vadd,
             kanji=content.kanji,
-            id=content.id
+            id=content.id,
+            utf=content.utf
         )
 
     def _cell(self, content, icol):
@@ -88,6 +172,10 @@ class LatexKanjiTable(object):
 if __name__ == "__main__":
     k = Kanjis("data/kanjis.csv")
     lt = LatexKanjiTable(k)
+    # ts = TangorinScraper()
+    # ts.download_kanjis(k.kanjis)
+    tp = TangorinParser()
+    tp.save2csv(tp.parse_dir("scrape/"))
     # lt.ncols = 10
-    with open("build/table.tex", "w") as outfile:
-        outfile.write(lt.generate())
+    # with open("build/table.tex", "w") as outfile:
+    #     outfile.write(lt.generate())
