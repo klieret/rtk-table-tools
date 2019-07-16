d56adbcc3da3259a72edabce39da2e6569d78226
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-07-16 18:40:07 +0200

    [Feat] Small CLI

--- generate.py
+++ generate.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# std
+import argparse
+
 # ours
 from rtktools.kanjicollection import KanjiCollection
 from rtktools.scraper.tangorin.parser import TangorinParser
@@ -7,13 +10,43 @@ from rtktools.scraper.tangorin.scraper import TangorinScraper
 from rtktools.poster import KanjiPoster
 
 
-if __name__ == "__main__":
+def usage(args):
+    print("Use --help to show usage!")
+
+
+def poster(args):
     k = KanjiCollection("data/kanjis.csv")
-    # kp = KanjiPoster(k)
-    # ts = TangorinScraper()
-    # ts.download_kanjis(k.kanjis)
+    p = KanjiPoster(k)
+    with open("build/table.tex", "w") as outfile:
+        outfile.write(p.generate())
+
+
+def scrape(args):
+    k = KanjiCollection("data/kanjis.csv")
+    ts = TangorinScraper()
+    ts.download_kanjis(k.kanjis)
+
+
+def parse(args):
     tp = TangorinParser()
     tp.save2csv(tp.parse_dir("scrape/raw/"))
-    # lt.ncols = 10
-    # with open("build/table.tex", "w") as outfile:
-    #     outfile.write(lt.generate())
+
+
+def cli():
+    parser = argparse.ArgumentParser(description="RTK Tools")
+    subparsers = parser.add_subparsers(
+        title="Subcommands"
+    )
+    parser.set_defaults(func=usage)
+    poster_parser = subparsers.add_parser("poster")
+    poster_parser.set_defaults(func=poster)
+    scrape_parser = subparsers.add_parser("scrape")
+    scrape_parser.set_defaults(func=scrape)
+    parser_parser = subparsers.add_parser("parse")
+    parser_parser.set_defaults(func=parse)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    cli()
